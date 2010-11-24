2a500b4a5580b60a52aac3f9a1f59239f9d37bc0
Sri Prasanna <k.sriprasanna@gmail.com>
2010-11-24 19:25:15 +0530

    modifications

--- install.sh
+++ install.sh
@@ -2,16 +2,18 @@ mkdir install && cd install
 yum -y install curl gcc memcached sqlite3 xfsprogs python-setuptools python-devel python-simplejson python-configobj python-nose
 mkdir packages && cd packages
 wget http://pypi.python.org/packages/source/W/WebOb/WebOb-0.9.8.tar.gz -O WebOb.tar.gz
-tar xvfz WebOb
-cd WebOb-0.9.8.tar.gz
+tar xvfz WebOb.tar.gz
+cd WebOb
 python setup.py install
 cd ..
 easy_install eventlet
 wget http://pypi.python.org/packages/source/x/xattr/xattr-0.6.1.tar.gz -O xattr.tar.gz
+tar xvfz xattr.tar.gz
 cd xattr 
 python setup.py install
 cd ..
 wget http://pypi.python.org/packages/source/c/coverage/coverage-3.4.tar.gz -O coverage.tar.gz
+tar xvfz coverage.tar.gz
 cd coverage
 python setup.py install
 cd ..
